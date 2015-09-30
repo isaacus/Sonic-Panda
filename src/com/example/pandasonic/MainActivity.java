2cd4e8f4bd8064ad468fb7b432339bc9b8993cf7
isaacus <skywalkerlyl@gmail.com>
2015-09-29 22:06:58 -0500

    MapNavigation update 9/28/2015
    1. Enhance the application with google direction api, basic route plan
    api call
    
    2. Furture plan: optimization with our own route planning scheme

--- src/com/example/pandasonic/MainActivity.java
+++ src/com/example/pandasonic/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.pandasonic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -10,6 +13,8 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
+	static final int NAVIGATION_REQUEST = 1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -52,13 +57,41 @@ public class MainActivity extends Activity {
 	
 	public void mapNavigation(View view) {
 		//Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
-		Uri gmmIntentUri = Uri.parse("google.navigation:q=806+Linda+Vista+Ave,+Arlington+Texas");
+		Uri gmmIntentUri = Uri.parse("google.navigation:q=1004+w+4th+st,+Arlington+Texas");
 		
 		Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
 		
 		mapIntent.setPackage("com.google.android.apps.maps");
 		
-		if(mapIntent.resolveActivity(getPackageManager()) != null)
-			startActivity(mapIntent);
+		if(mapIntent.resolveActivity(getPackageManager()) != null) {
+			System.out.println("Navigation start");
+			startActivityForResult(mapIntent, NAVIGATION_REQUEST);
+		}
+	}
+	
+	public void getRoute(View view) {
+		//https://maps.googleapis.com/maps/api/directions/output?parameters
+		try {
+			URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyDYZurWyusHaKrHIe5RpQ3ooArKSJ-031M");
+		
+			RoutePlanTask routePlanTask = new RoutePlanTask(this);
+			routePlanTask.execute(url);
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == NAVIGATION_REQUEST) {
+			System.out.println("Navigation Request!");
+			if(resultCode == RESULT_OK)
+				System.out.println("Navigation Finished!");
+			else if(resultCode == RESULT_CANCELED)
+				System.out.println("Navigation Canceled!");
+		}
 	}
 }
